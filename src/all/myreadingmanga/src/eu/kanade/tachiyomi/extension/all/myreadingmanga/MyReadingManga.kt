7e2db59f67e5dd9e2d78e71cbcab32d7d18227db
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-22 15:40:13 -0300

    Update MyReadingManga.kt

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
@@ -427,7 +427,6 @@ open class MyReadingManga(override val lang: String, private val siteLang: Strin
         private const val USER_AGENT = "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36"
         private const val USERNAME_PREF = "MYREADINGMANGA_USERNAME"
         private const val PASSWORD_PREF = "MYREADINGMANGA_PASSWORD"
-        private const val TAG = "MyReadingMangaLogin"
     }
 
     private fun randomString(length: Int): String {

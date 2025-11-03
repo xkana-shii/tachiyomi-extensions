f6d46b27ee9339d3e59e83243c35e95b8816478c
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-11-03 10:56:39 -0300

    Swap Japanese source language codes

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingMangaFactory.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingMangaFactory.kt
@@ -31,7 +31,7 @@ private val languageList = listOf(
 //    Source("hi", "Hindi"),
     Source("hu", "Hungarian"),
     Source("it", "Italian"),
-    Source("ja", "Japanese", "jp"),
+    Source("jp", "Japanese", "ja"),
     Source("ko", "Korean"),
     Source("lt", "Lithuanian"),
 //    Source("ms", "Malay"),

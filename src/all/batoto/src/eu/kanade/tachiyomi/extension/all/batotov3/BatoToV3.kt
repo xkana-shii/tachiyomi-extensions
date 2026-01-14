7b89fec7c06971d30a4dfd6e63b822c65381b6f0
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-01-14 19:58:27 -0300

    v3 part

---
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batotov3/BatoToV3.kt
@@ -0,0 +1,308 @@
+package eu.kanade.tachiyomi.extension.all.batotov3
+
+import android.app.Application
+import android.content.SharedPreferences
+import android.widget.Toast
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.network.POST
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.HttpSource
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import rx.Observable
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+open class BatoTo(
+    final override val lang: String,
+    private val siteLang: List<String>,
+) : ConfigurableSource, HttpSource() {
+
+    private val preferences: SharedPreferences by lazy {
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
+    }
+
+    override val name: String = "Bato.to V3"
+
+    override val baseUrl: String = getMirrorPref()!!
+
+    override val id: Long = when (lang) {
+        "zh-Hans" -> 2818874445640189582
+        "zh-Hant" -> 38886079663327225
+        "ro-MD" -> 8871355786189601023
+        else -> super.id
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        ListPreference(screen.context).apply {
+            key = "${MIRROR_PREF_KEY}_$lang"
+            title = MIRROR_PREF_TITLE
+            entries = MIRROR_PREF_ENTRIES
+            entryValues = MIRROR_PREF_ENTRY_VALUES
+            setDefaultValue(MIRROR_PREF_DEFAULT_VALUE)
+            summary = "%s"
+
+            setOnPreferenceChangeListener { _, newValue ->
+                val selected = newValue as String
+                val index = findIndexOfValue(selected)
+                val entry = entryValues[index] as String
+                Toast.makeText(screen.context, RESTART_TACHIYOMI, Toast.LENGTH_LONG).show()
+                preferences.edit().putString("${MIRROR_PREF_KEY}_$lang", entry).commit()
+            }
+        }.let { screen.addPreference(it) }
+
+        ListPreference(screen.context).apply {
+            key = "${COVER_PREF_KEY}_$lang"
+            title = COVER_PREF_TITLE
+            entries = COVER_PREF_ENTRIES
+            entryValues = COVER_PREF_ENTRIES
+            setDefaultValue(COVER_PREF_DEFAULT_VALUE)
+            summary = "%s"
+        }.let { screen.addPreference(it) }
+    }
+
+    private fun getMirrorPref(): String? = preferences.getString("${MIRROR_PREF_KEY}_$lang", MIRROR_PREF_DEFAULT_VALUE)
+
+    private fun coverQuality(): CoverQuality {
+        return when (preferences.getString("${COVER_PREF_KEY}_$lang", COVER_PREF_DEFAULT_VALUE)) {
+            "Medium" -> CoverQuality.Medium
+            "Low" -> CoverQuality.Low
+            else -> CoverQuality.Original
+        }
+    }
+
+    override val supportsLatest = true
+
+    private val json: Json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+        encodeDefaults = true
+        coerceInputValues = true
+    }
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(4)
+        .build()
+
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", "$baseUrl/")
+
+    override fun popularMangaRequest(page: Int) = searchMangaRequest(page, "", FilterList(SortFilter("field_score")))
+
+    override fun popularMangaParse(response: Response) = searchMangaParse(response)
+
+    override fun latestUpdatesRequest(page: Int) = searchMangaRequest(page, "", FilterList(SortFilter("field_upload")))
+
+    override fun latestUpdatesParse(response: Response) = searchMangaParse(response)
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        if (query.startsWith(SEARCH_PREFIX)) {
+            val id = query.substringAfter(SEARCH_PREFIX)
+            val manga = SManga.create().apply { url = "/series/$id/" }
+            return fetchMangaDetails(manga).map {
+                MangasPage(listOf(it), false)
+            }
+        }
+        return super.fetchSearchManga(page, query, filters)
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        val payloadObj = ApiSearchPayload(
+            pageNumber = page,
+            size = 30,
+            query = query.trim(),
+            incTLangs = siteLang,
+            sort = filters.firstInstanceOrNull<SortFilter>()?.selected,
+            incGenres = filters.firstInstanceOrNull<GenreGroupFilter>()?.included,
+            excGenres = filters.firstInstanceOrNull<GenreGroupFilter>()?.excluded,
+            origStatus = filters.firstInstanceOrNull<OriginalStatusFilter>()?.selected,
+            batoStatus = filters.firstInstanceOrNull<BatoStatusFilter>()?.selected,
+            incOLangs = filters.firstInstanceOrNull<OriginGroupFilter>()?.selected,
+            chapCount = filters.firstInstanceOrNull<ChapterCountFilter>()?.selected,
+        )
+
+        return apiRequest(payloadObj)
+    }
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        val searchResponse = response.parseAs<ApiSearchResponse>()
+
+        val hasNextPage = searchResponse.data.search.paging.let {
+            it.total > it.page
+        }
+
+        return searchResponse.data.search.items
+            .map { items ->
+                items.data.toSManga(coverQuality())
+                    .apply { initialized = true }
+            }
+            .let { MangasPage(it, hasNextPage) }
+    }
+
+    override fun getFilterList() = filters
+
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        val id = manga.url.split("/")[2]
+
+        val payloadObj = ApiQueryPayload(id, DETAILS_QUERY)
+
+        return apiRequest(payloadObj)
+    }
+
+    override fun mangaDetailsParse(response: Response): SManga {
+        val mangaData = response.parseAs<ApiDetailsResponse>()
+
+        return mangaData.data.comicNode.data.toSManga(coverQuality())
+    }
+
+    override fun getMangaUrl(manga: SManga): String {
+        val url = manga.url.split("/")
+        return "$baseUrl/title/${url[2]}-${url[3]}"
+    }
+
+    override fun chapterListRequest(manga: SManga): Request {
+        val id = manga.url.split("/")[2]
+
+        val payloadObj = ApiQueryPayload(id, CHAPTERS_QUERY)
+
+        return apiRequest(payloadObj)
+    }
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val chapterList = response.parseAs<ApiChapterListResponse>()
+
+        return chapterList.data.chapters
+            .map { it.data.toSChapter() }
+            .sortedByDescending { it.chapter_number }
+    }
+
+    override fun getChapterUrl(chapter: SChapter): String {
+        return "$baseUrl/${chapter.url}"
+    }
+
+    override fun pageListRequest(chapter: SChapter): Request {
+        val id = chapter.url
+            .removeSuffix("/")
+            .substringAfterLast("/")
+            .substringBefore("-")
+
+        val payloadObj = ApiQueryPayload(id, PAGES_QUERY)
+
+        return apiRequest(payloadObj)
+    }
+
+    override fun pageListParse(response: Response): List<Page> {
+        val pages = response.parseAs<ApiPageListResponse>()
+
+        return pages.data.pageList.data.imageFiles?.mapIndexed { index, image ->
+            Page(index, "", image)
+        } ?: emptyList()
+    }
+
+    override fun imageUrlParse(response: Response): String {
+        throw UnsupportedOperationException("Not Used")
+    }
+
+    private inline fun <reified R> List<*>.firstInstanceOrNull(): R? =
+        filterIsInstance<R>().firstOrNull()
+
+    private inline fun <reified T> Response.parseAs(): T = json.decodeFromString(body.string())
+
+    private inline fun <reified T> apiRequest(payloadObj: T): Request {
+        val payload = json.encodeToString(payloadObj)
+            .toRequestBody(JSON_MEDIA_TYPE)
+
+        val apiHeaders = headersBuilder()
+            .add("Content-Length", payload.contentLength().toString())
+            .add("Content-Type", payload.contentType().toString())
+            .build()
+
+        return POST("$baseUrl/apo", apiHeaders, payload)
+    }
+
+    companion object {
+        private val JSON_MEDIA_TYPE = "application/json; charset=utf-8".toMediaTypeOrNull()
+        val DATE_FORMATTER by lazy {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
+        }
+        val chapterNumRegex by lazy { Regex("""\.0+$""") }
+        const val SEARCH_PREFIX = "ID:"
+        private const val RESTART_TACHIYOMI = "Restart Tachiyomi to apply new setting."
+        private const val COVER_PREF_KEY = "COVER"
+        private const val COVER_PREF_TITLE = "Cover Quality"
+        private val COVER_PREF_ENTRIES = arrayOf("Original", "Medium", "Low")
+        private val COVER_PREF_DEFAULT_VALUE = COVER_PREF_ENTRIES[0]
+        private const val MIRROR_PREF_KEY = "MIRROR"
+        private const val MIRROR_PREF_TITLE = "Preferred Mirror"
+        private val MIRROR_PREF_ENTRIES = arrayOf(
+            "bato.to",
+            "wto.to",
+            "mto.to",
+            "dto.to",
+            "hto.to",
+            "batotoo.com",
+            "battwo.com",
+            "batotwo.com",
+            "comiko.net",
+            "mangatoto.com",
+            "mangatoto.net",
+            "mangatoto.org",
+            "comiko.org",
+            "batocomic.com",
+            "batocomic.net",
+            "batocomic.org",
+            "readtoto.com",
+            "readtoto.net",
+            "readtoto.org",
+            "xbato.com",
+            "xbato.net",
+            "xbato.org",
+            "zbato.com",
+            "zbato.net",
+            "zbato.org",
+        )
+        private val MIRROR_PREF_ENTRY_VALUES = arrayOf(
+            "https://bato.to",
+            "https://wto.to",
+            "https://mto.to",
+            "https://dto.to",
+            "https://hto.to",
+            "https://batotoo.com",
+            "https://battwo.com",
+            "https://batotwo.com",
+            "https://comiko.net",
+            "https://mangatoto.com",
+            "https://mangatoto.net",
+            "https://mangatoto.org",
+            "https://comiko.org",
+            "https://batocomic.com",
+            "https://batocomic.net",
+            "https://batocomic.org",
+            "https://readtoto.com",
+            "https://readtoto.net",
+            "https://readtoto.org",
+            "https://xbato.com",
+            "https://xbato.net",
+            "https://xbato.org",
+            "https://zbato.com",
+            "https://zbato.net",
+            "https://zbato.org",
+        )
+        val MIRROR_PREF_DEFAULT_VALUE = MIRROR_PREF_ENTRY_VALUES[0]
+    }
+}
---
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batotov3/Dto.kt
@@ -0,0 +1,292 @@
+package eu.kanade.tachiyomi.extension.all.batotov3
+
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.long
+import java.util.Locale
+
+@Serializable
+data class ApiSearchResponse(
+    val data: SearchData,
+) {
+    @Serializable
+    data class SearchData(
+        @SerialName("get_content_searchComic") val search: SearchComic,
+    ) {
+        @Serializable
+        data class SearchComic(
+            val paging: PageInfo,
+            val items: List<SearchItems>,
+        ) {
+            @Serializable
+            data class PageInfo(
+                @SerialName("pages") val total: Int,
+                val page: Int,
+            )
+
+            @Serializable
+            data class SearchItems(
+                val data: SeriesDto,
+            )
+        }
+    }
+}
+
+@Serializable
+data class ApiDetailsResponse(
+    val data: ComicNode,
+) {
+    @Serializable
+    data class ComicNode(
+        @SerialName("get_content_comicNode") val comicNode: MangaData,
+    ) {
+        @Serializable
+        data class MangaData(
+            val data: SeriesDto,
+        )
+    }
+}
+
+@Serializable
+data class SeriesDto(
+    val id: String,
+    val name: String,
+    val slug: String,
+    val summary: SummaryDto?,
+    val altNames: List<String>? = emptyList(),
+    val authors: List<String>? = emptyList(),
+    val artists: List<String>? = emptyList(),
+    val genres: List<String>? = emptyList(),
+    val originalStatus: String?,
+    val uploadStatus: String?,
+    @SerialName("urlCoverOri") val coverOriginal: String?,
+    @SerialName("urlCover600") val coverMedium: String?,
+    @SerialName("urlCover300") val coverLow: String?,
+) {
+    fun toSManga(cover: CoverQuality = CoverQuality.Original): SManga = SManga.create().apply {
+        title = name.trim()
+        url = "/series/$id/$slug"
+        author = authors?.joinToString { it.trim() }
+        artist = artists?.joinToString { it.trim() }
+        description = summary?.text?.trim()
+        if (!altNames.isNullOrEmpty()) {
+            description += altNames.joinToString(
+                prefix = "\n\nAlternative Names: \n* ",
+                separator = "\n* ",
+            ) { it.trim() }
+        }
+        genre = genres?.joinToString { genre ->
+            genre
+                .replace("_", " ")
+                .replaceFirstChar {
+                    if (it.isLowerCase()) {
+                        it.titlecase(Locale.ROOT)
+                    } else {
+                        it.toString()
+                    }
+                }
+        }
+        thumbnail_url = when (cover) {
+            CoverQuality.Medium -> coverMedium
+            CoverQuality.Low -> coverLow
+            else -> coverOriginal
+        }
+        status = parseStatus(originalStatus, uploadStatus)
+    }
+
+    private fun parseStatus(originalStatus: String?, uploadStatus: String?): Int {
+        return when (originalStatus) {
+            null -> SManga.UNKNOWN
+            "ongoing" -> SManga.ONGOING
+            "cancelled" -> SManga.CANCELLED
+            "hiatus" -> SManga.ON_HIATUS
+            "completed" -> when (uploadStatus) {
+                "ongoing" -> SManga.PUBLISHING_FINISHED
+                else -> SManga.COMPLETED
+            }
+            else -> SManga.UNKNOWN
+        }
+    }
+
+    @Serializable
+    data class SummaryDto(
+        val text: String?,
+    )
+}
+
+enum class CoverQuality {
+    Original,
+    Medium,
+    Low,
+}
+
+@Serializable
+data class ApiChapterListResponse(
+    val data: ChapterList,
+) {
+    @Serializable
+    data class ChapterList(
+        @SerialName("get_content_chapterList") val chapters: List<ChapterData>,
+    ) {
+        @Serializable
+        data class ChapterData(
+            val data: ChapterDto,
+        ) {
+            @Serializable
+            data class ChapterDto(
+                val id: String,
+                val title: String? = null,
+                val chaNum: Float,
+                val urlPath: String,
+                val dateCreate: JsonPrimitive? = null,
+                val dateModify: JsonPrimitive? = null,
+                @SerialName("userNode") val user: ScanlatorNode? = null,
+                @SerialName("groupNodes") val groups: List<ScanlatorNode>? = emptyList(),
+            ) {
+                fun toSChapter() = SChapter.create().apply {
+                    url = urlPath
+                    name = "Chapter ${chaNum.parseChapterNumber()}"
+                    if (!title.isNullOrEmpty()) {
+                        name += ": $title"
+                    }
+                    chapter_number = chaNum
+                    if (!groups.isNullOrEmpty()) {
+                        scanlator = groups
+                            .filterNot { it.data.name.isNullOrEmpty() }
+                            .joinToString { it.data.name?.trim().toString() }
+                    } else if (user != null && user.data.name != null) {
+                        scanlator = "Uploaded by ${user.data.name.trim()}"
+                    }
+                    date_upload = dateModify?.parseDate() ?: dateCreate?.parseDate() ?: 0L
+                }
+
+                private fun Float.parseChapterNumber(): String {
+                    return this.toString().replace(BatoTo.chapterNumRegex, "")
+                }
+
+                private fun JsonPrimitive.parseDate(): Long? {
+                    // api sometimes return string and sometimes long 🗿
+                    return runCatching {
+                        if (this.isString) {
+                            BatoTo.DATE_FORMATTER.parse(this.toString())!!.time
+                        } else {
+                            return this.long
+                        }
+                    }.getOrNull()
+                }
+
+                @Serializable
+                data class ScanlatorNode(
+                    val data: NameDto,
+                ) {
+                    @Serializable
+                    data class NameDto(
+                        val name: String? = null,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Serializable
+data class ApiPageListResponse(
+    val data: ChapterNode,
+) {
+    @Serializable
+    data class ChapterNode(
+        @SerialName("get_content_chapterNode") val pageList: PageList,
+    ) {
+        @Serializable
+        data class PageList(
+            val data: PageData,
+        ) {
+            @Serializable
+            data class PageData(
+                val imageFiles: List<String>? = emptyList(),
+            )
+        }
+    }
+}
+
+@Serializable
+data class ApiSearchPayload(
+    val variables: Variables,
+    val query: String,
+) {
+    @Serializable
+    data class Variables(
+        val select: Select,
+    )
+
+    @Serializable
+    data class Select(
+        val page: Int,
+        val size: Int,
+        val where: String,
+        val word: String,
+        val sort: String,
+        val incGenres: List<String>,
+        val excGenres: List<String>,
+        val incOLangs: List<String>,
+        val incTLangs: List<String>,
+        val origStatus: String,
+        val batoStatus: String,
+        val chapCount: String,
+    )
+
+    constructor(
+        pageNumber: Int,
+        size: Int,
+        sort: String?,
+        query: String = "",
+        where: String = "browse",
+        incGenres: List<String>? = emptyList(),
+        excGenres: List<String>? = emptyList(),
+        incOLangs: List<String>? = emptyList(),
+        incTLangs: List<String>? = emptyList(),
+        origStatus: String? = "",
+        batoStatus: String? = "",
+        chapCount: String? = "",
+    ) : this(
+        Variables(
+            Select(
+                page = pageNumber,
+                size = size,
+                where = where,
+                word = query,
+                sort = sort ?: "",
+                incGenres = incGenres ?: emptyList(),
+                excGenres = excGenres ?: emptyList(),
+                incOLangs = incOLangs ?: emptyList(),
+                incTLangs = incTLangs ?: emptyList(),
+                origStatus = origStatus ?: "",
+                batoStatus = batoStatus ?: "",
+                chapCount = chapCount ?: "",
+            ),
+        ),
+        SEARCH_QUERY,
+    )
+}
+
+@Serializable
+data class ApiQueryPayload(
+    val variables: Variables,
+    val query: String,
+) {
+    @Serializable
+    data class Variables(
+        val id: String,
+    )
+
+    constructor(
+        id: String,
+        query: String,
+    ) : this(
+        Variables(id),
+        query,
+    )
+}

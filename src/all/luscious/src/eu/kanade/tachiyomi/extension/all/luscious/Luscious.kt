e842e628850a33d75d3a932bff00e6e25bf9a91f
CriosChan <36739192+CriosChan@users.noreply.github.com>
2026-03-11 14:04:17 +0100

    Luscious: Fix gif issue (#13908)

--- src/all/luscious/src/eu/kanade/tachiyomi/extension/all/luscious/Luscious.kt
+++ src/all/luscious/src/eu/kanade/tachiyomi/extension/all/luscious/Luscious.kt
@@ -331,6 +331,9 @@ abstract class Luscious(
                             getResolutionPref() != "-1" -> {
                                 it.jsonObject["thumbnails"]!!.jsonArray[getResolutionPref()?.toInt()!!].jsonObject["url"]!!.jsonPrimitive.content
                             }
+                            it.jsonObject["url_to_video"]!!.jsonPrimitive.contentOrNull != null -> {
+                                it.jsonObject["url_to_video"]!!.jsonPrimitive.content.toHttpUrl().newBuilder().host("ah-img.luscious.net").build().toString().replace(".mp4", ".gif")
+                            }
                             it.jsonObject["url_to_original"]!!.jsonPrimitive.contentOrNull != null -> {
                                 it.jsonObject["url_to_original"]!!.jsonPrimitive.content
                             }
@@ -435,6 +438,9 @@ abstract class Luscious(
                 val url = when {
                     getResolutionPref() != "-1" -> {
                         it.jsonObject["thumbnails"]!!.jsonArray[getResolutionPref()?.toInt()!!].jsonObject["url"]!!.jsonPrimitive.content
+                    }
+                    it.jsonObject["url_to_video"]!!.jsonPrimitive.contentOrNull != null -> {
+                        it.jsonObject["url_to_video"]!!.jsonPrimitive.content.toHttpUrl().newBuilder().host("ah-img.luscious.net").build().toString().replace(".mp4", ".gif")
                     }
                     it.jsonObject["url_to_original"]!!.jsonPrimitive.contentOrNull != null -> {
                         it.jsonObject["url_to_original"]!!.jsonPrimitive.content
@@ -897,6 +903,7 @@ abstract class Luscious(
                         created
                         title
                         url_to_original
+                        url_to_video
                         position
                         thumbnails {
                             url

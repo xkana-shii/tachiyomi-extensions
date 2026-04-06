3091b72ae2ac1fe59a9254fc57395874f202fa7e
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-04-06 08:48:52 +0700

    YuriNeko (VI): Support related manga (#403)
    * YuriNeko (VI): Support related manga
    
    * Fix related manga list request by updating manga ID extraction method

--- src/vi/yurineko/src/eu/kanade/tachiyomi/extension/vi/yurineko/YuriNeko.kt
+++ src/vi/yurineko/src/eu/kanade/tachiyomi/extension/vi/yurineko/YuriNeko.kt
@@ -286,10 +286,19 @@ class YuriNeko : HttpSource() {
     // =============================== Related ================================
 
     // dirty hack to disable suggested mangas on Komikku due to heavy rate limit
-    // https://github.com/komikku-app/komikku/blob/4323fd5841b390213aa4c4af77e07ad42eb423fc/source-api/src/commonMain/kotlin/eu/kanade/tachiyomi/source/CatalogueSource.kt#L176-L184
-    @Suppress("Unused")
-    @JvmName("getDisableRelatedMangasBySearch")
-    fun disableRelatedMangasBySearch() = true
+    override val disableRelatedMangasBySearch = true
+
+    override fun relatedMangaListRequest(manga: SManga): Request {
+        val mangaId = "$baseUrl${manga.url}"
+            .toHttpUrl()
+            .mangaId()
+        return GET("$apiUrl/mangas/$mangaId/related", headers)
+    }
+
+    override fun relatedMangaListParse(response: Response): List<SManga> {
+        val related = response.parseAs<List<MangaDto>>()
+        return related.map(::mangaFromDto)
+    }
 
     companion object {
         private const val POPULAR_LIMIT = 10

8bff698392fb51b3d679f4ec1e5afa7740d6c8e8
kana-shii <deiimos.mars@gmail.com>
2026-07-10 19:01:35 -0300

    [Lezhin]: New source
    WIP, doesn't work properly

---
+++ src/all/lezhin/src/eu/kanade/tachiyomi/extension/all/lezhin/LezhinEN.kt
@@ -0,0 +1,6 @@
+package eu.kanade.tachiyomi.extension.all.lezhin
+
+class LezhinEN : Lezhin("en", "https://www.lezhinus.com", "Lezhin") {
+    override val pathSegment: String = "en"
+    override val siteLocale: String = "en-US"
+}
---
+++ src/all/lezhin/src/eu/kanade/tachiyomi/extension/all/lezhin/LezhinKO.kt
@@ -0,0 +1,6 @@
+package eu.kanade.tachiyomi.extension.all.lezhin
+
+class LezhinKO : Lezhin("ko", "https://www.lezhin.com", "Lezhin") {
+    override val pathSegment: String = "ko"
+    override val siteLocale: String = "ko-KR"
+}

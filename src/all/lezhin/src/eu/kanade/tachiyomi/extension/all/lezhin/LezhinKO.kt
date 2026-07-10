3fdac52be0587e29216018b285b6fe81de5d6c2b
kana-shii <deiimos.mars@gmail.com>
2026-07-10 20:02:01 -0300

    Revert "[Lezhin]: New source"
    This reverts commit c907a487dd07d0286f7c4832388c79f9a4fa5f47.

--- src/all/lezhin/src/eu/kanade/tachiyomi/extension/all/lezhin/LezhinKO.kt
+++
@@ -1,6 +0,0 @@
-package eu.kanade.tachiyomi.extension.all.lezhin
-
-class LezhinKO : Lezhin("ko", "https://www.lezhin.com", "Lezhin") {
-    override val pathSegment: String = "ko"
-    override val siteLocale: String = "ko-KR"
-}
